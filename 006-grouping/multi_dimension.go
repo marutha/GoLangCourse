1e7cb06cbd84373972aa8d580194cfb7604ecbc9
maru <maruthavanan.s@gmail.com>
2020-04-22 11:10:11 +0530

    Multi dimensional array

---
+++ 006-grouping/multi_dimension.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	jb := []string{"james", "bond", "c", "m"}
+	mb := []string{"money", "penny", "a", "b"}
+
+	xp := [][]string{jb, mb}
+	fmt.Println(xp)
+}
